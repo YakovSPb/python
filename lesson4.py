65eae3795653e6778aba696e5b8b4fc35c1ad20e
Yakov <itmeetm@gmail.com>
2025-10-12 17:39:21 +0300

    lesson4

---
+++ lesson4.py
@@ -0,0 +1,27 @@
+# number = 5
+
+# digit = 4.5
+# word = 'Результет:'
+# bool = True
+
+# str_num = '5'
+
+# # print(word + str(digit))
+# print(number + int(str_num))
+
+# del number
+
+# number = 7
+
+# print(word, number)
+
+num1 = int(input("Введите первое число: "))
+num2 = int(input("Введите второе число: "))
+
+print("Result:", num1 + num2)
+print("Result:", num1 - num2)
+print("Result:", num1 / num2)
+print("Result:", num1 * num2)
+
+word = 'hi'
+print(word * 2)
