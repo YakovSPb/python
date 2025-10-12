1ec190a44355754d60ea00f7e77007b49339dc02
Yakov <itmeetm@gmail.com>
2025-10-12 15:30:12 +0300

    lesson 3

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-print("Hello word")
+# lesson 3
 
-print(5+5)
+# print("Hello word")
+
+# print("Результат: ", 7, sep="", end="!\n")
+
+# print("Second \"line")
+
+print("Результат:", max(1,2,4,1212,121))
+
+input("Введите свой возраст: ")
