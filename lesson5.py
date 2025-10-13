6f47ca178e82a0ebd147af7a512c0abc358e7720
Yakov <itmeetm@gmail.com>
2025-10-13 12:09:13 +0300

    lesson 5

---
+++ lesson5.py
@@ -0,0 +1,36 @@
+
+
+# isHappy = True
+# number = int(input('Введите число '))
+
+# if not isHappy or number == 6:
+#     print("User not Happy")
+# else:
+#     print("User Happy")
+
+
+# if not isHappy and number == 6:
+#     print("User not Happy")
+# else:
+#     print("User Happy")
+
+
+# if number == 6:
+#     print("yes 6")
+#     print("!!!")
+# elif number > 6:
+#     print('больше 6')   
+# else:
+#     print('меньше 6')   
+
+
+data = input()
+
+number = 5 if data == "Five" else 0
+
+# if data == "Five":
+#     number = 5
+# else:
+#     number = 0
+
+print(number)
