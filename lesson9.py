fe3600645e3a9deada58cbd5f48f1fa275d0eb34
Yakov <itmeetm@gmail.com>
2025-10-14 17:15:26 +0300

    lesson 9

---
+++ lesson9.py
@@ -0,0 +1,19 @@
+# Кортежи
+# data = (4,6,6,4,3,True)
+
+# # data[0] = 5 -no
+# # print(data[1])
+
+# # print(data.count(6))
+
+
+data = 5,6, True
+
+# data = (4,6,6,4,3,True)
+
+for el in data:
+    print(el)
+
+nums = [5, 7, 8]
+
+new_data = tuple(nums)
