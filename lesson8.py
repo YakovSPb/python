1ecd88f7fecedb71573b811385ce30c885372a68
Yakov <itmeetm@gmail.com>
2025-10-14 14:23:51 +0300

    lesson 8

---
+++ lesson8.py
@@ -0,0 +1,25 @@
+# word = 'yakov kondratev'
+# word = word.split(' ')
+
+# # print(word.upper())
+# # print(word.isupper())
+# # print(word.find('k'))
+
+# # print(word.split(' '))
+
+
+# for i in range(len(word)):
+#     word[i] = word[i].capitalize()
+
+
+# result = ", ".join(word)
+
+# print(result)
+
+# word = 'Football'
+
+# print(word[1:-1:2])
+
+list = [6,4, 'string', True, 6.5]
+
+print(list[2:])
