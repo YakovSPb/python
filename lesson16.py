756c2af4a58613bdae359b24330a4796fa1aee30
Yakov <itmeetm@gmail.com>
2025-10-15 19:09:47 +0300

    lesson 16

---
+++ lesson16.py
@@ -0,0 +1,14 @@
+import datetime as d, sys, os, platform
+from math import sqrt as s, ceil
+import moduls as my
+from moduls import add_three_numbers as add
+import cowsay
+
+# print(d.datetime.now().time())
+# print(sys)
+# print(platform.system())
+# print(ceil(s(100)))
+
+print(my.name)
+print(add(1,2,0))
+cowsay.cow('Fuck off')
