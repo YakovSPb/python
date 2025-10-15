fff390d33749fb6d3ba4685554be6e2378bd0878
Yakov <itmeetm@gmail.com>
2025-10-15 12:38:06 +0300

    lesson 12

---
+++ lesson12.py
@@ -0,0 +1,32 @@
+# def test_func(word):
+#     print(word, end="")
+#     print("!")
+
+# test_func("Hi")
+# test_func(5)
+
+# def summ(a,b):
+#     return a+b
+
+# res = summ(2,5)
+# print(res)
+
+
+# nums1 = [5,7,9,4]
+
+# def minimal(arr):
+#     result = nums1[0]
+
+#     for num in nums1:
+#         if num < result:
+#             result = num
+    
+#     return result
+
+# print(minimal(nums1))
+
+# def minimal(l):
+
+func = lambda x, y: x * y
+
+print(func(5,10))
